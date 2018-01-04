d7f1093e4ffd28f4f96d0bf6b280d72308782253
Scott Sapp <scottiesapp2@gmail.com>
2018-01-04 02:03:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === lowercase); return ("I can\'t hear you!");
-    if (string === uppercase); return ("YES INDEED!");
+  if (string === string.toLowerCase()) { return ("I can\'t hear you!")}
+    if (string === string.toUpperCase()) {  return ("YES INDEED!")}
       if (string === "I love you, Grandma."); return "I love you, too.";
 }
